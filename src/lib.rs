f38492b1530d00006cee314b7553fb55211b4556
bilesimo <stang3tzzz@gmail.com>
2024-10-09 22:23:07 -0300

    add task no2

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #[cfg(test)] 
 mod tests {
-  use solana_sdk::signature::{Keypair, Signer};
+  use solana_sdk::signature::{Keypair, Signer, read_keypair_file};
   use bs58;
-  use std::io::{self, BufRead};
+  use std::{io::{self, BufRead}, thread::sleep, time::Duration};
+  use solana_client::rpc_client::RpcClient;
+
+  const RPC_URL: &str = "https://api.devnet.solana.com";
 
   #[test]
   fn keygen() {
@@ -32,9 +35,53 @@ mod tests {
     println!("Your private key is:");
     println!("{:?}", base58);
   }
+
+  #[test]
+  fn public_key() {
+    let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+    println!("Your public key is: {}", keypair.pubkey().to_string());
+  }
   
   #[test]
-  fn airdop() {}
+  fn airdop() {
+    let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+    let client = RpcClient::new(RPC_URL);
+
+    let mut retries = 5; // Number of retry attempts
+    let mut delay = 2; // Initial delay in seconds
+
+    loop {
+        match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
+            Ok(s) => {
+                println!("Success! Check out your TX here:");
+                println!(
+                    "https://explorer.solana.com/tx/{}?cluster=devnet",
+                    s.to_string()
+                );
+                break;
+            }
+            Err(e) => {
+                if retries > 0 {
+                    println!("Airdrop failed due to rate limit. Retrying in {} seconds...", delay);
+                    sleep(Duration::from_secs(delay));
+                    delay *= 2; // Exponential backoff
+                    retries -= 1;
+                } else {
+                    println!("Failed after multiple retries. Error: {}", e.to_string());
+                    break;
+                }
+            }
+        };
+    }
+  }
+
+  #[test]
+  fn balance() {
+    let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+    let client = RpcClient::new(RPC_URL);
+    let balance = client.get_balance(&keypair.pubkey()).unwrap();
+    println!("Your balance is: {} SOL", balance as f64 / 1_000_000_000.0);
+  }
 
   #[test]
   fn transfer_sol() {}
